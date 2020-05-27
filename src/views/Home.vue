b45299f7743e13a08f15c737daf4b434de1cfb8b
Trevor Capozza <trevor.capozza@gmail.com>
2020-05-27 16:35:48 -0700

    dynamic dates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,25 +7,25 @@
       <div class="art">
         <img src="@/assets/data.svg" alt="">
         <p>
-          4 years experience creating in-depth data reports.
+          {{ new Date().getFullYear()-2016 }} years experience creating in-depth data reports.
         </p>
       </div>
       <div class="art">
         <img src="@/assets/developer.svg" alt="">
         <p>
-          8 years experience UI design and front-end development.
+          {{ new Date().getFullYear()-2012 }} years experience UI design and front-end development.
         </p>
       </div>
       <div class="art">
         <img src="@/assets/browser.svg" alt="">
         <p>
-          Managed over $750,000 in ad spend during 2019.
+          Managed over $750,000 in ad spend during {{ new Date().getFullYear()-1 }}.
         </p>
       </div>
     </div>
     <div class="skills max">
       <h1>Skills</h1>
-      <p>Over 7 years of design experience<br>using these programs.</p>
+      <p>Over {{ new Date().getFullYear()-2012 }} years of design experience<br>using these programs.</p>
       <div class="programs">
       <img src="@/assets/ps.svg" alt="">
       <img src="@/assets/ai.svg" alt="">

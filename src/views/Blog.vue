15a28ebff9ef9e4790fcd9c74fcd20b48b00af0d
Trevor Capozza <trevor.capozza@gmail.com>
2020-05-27 13:18:47 -0700

    broken link

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -7,7 +7,7 @@
       <p class="header">10 best design resources</p>
       <p class="timestamp">Feb 28, 2019</p>
       <p class="description">If you’re like me, you spend a lot of time trying to find the perfect pattern, the finest font, and the coolest color combo. Whether you’re just starting out or a veteran pixel champion, all these resources...</p>
-      <a href="" class="button">Read more</a>
+      <a href="/" class="button">Read more</a>
     </div>
   </div>
 </template>

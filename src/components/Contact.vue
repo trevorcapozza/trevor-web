5fb314a1d42833cee3737e4f1fabcaa345a50889
Trevor Capozza <trevor.capozza@gmail.com>
2020-05-27 11:43:53 -0700

    test

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true" action="/">
       <p>
         <label>Your Name: <input type="text" name="name" /></label>   
       </p>

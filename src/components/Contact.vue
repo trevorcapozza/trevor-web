2431f9ab5f5d3d495c3651ab19f51528f1bc516f
Trevor Capozza <trevor.capozza@gmail.com>
2020-05-27 14:41:49 -0700

    small update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="contact max">
     <h1>Contact</h1>
-    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+    <form name="Contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" placeholder="Name"/>   
         <input type="email" name="email" placeholder="Email"/>
@@ -14,6 +14,5 @@
 <script>
 export default {
   name: 'Contact',
-
 }
 </script>
